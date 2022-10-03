f01c0400e21c89848c38f65a6070d74244baf603
장영욱 <raeben@naver.com>
2022-10-03 20:00:56 +0900

    #sprint-1/3
    게시판 리스트 페이지 생성

---
+++ pages-pc/community-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <table class="jelly-table">
+      <tr>
+        <th>제목</th>
+        <th>이름</th>
+        <th>날짜</th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default-pc',
+}
+</script>
+
+<style></style>
