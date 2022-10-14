2467efeb737259cf86c20a52a4518151d83f039a
장영욱 <raeben@naver.com>
2022-10-14 10:11:49 +0900

    fix

---
+++ pages-mo/test-main-page.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="school-content">
+    <div class="content p-3">
+      컨텐츠
+      <!-- v-b-modal. 뒤에 모달창 아이디 -->
+      <button
+        class="jelly-btn jelly-btn--default"
+        @click="$bvModal.show('ModalNotice')"
+      >
+        모달 예시
+      </button>
+    </div>
+    <!-- 모달창 시작 -->
+    <b-modal id="ModalNotice" hide-footer>
+      <div>컨텐츠</div>
+      <div class="m-t-3">
+        <button
+          class="jelly-btn jelly-btn--default wd-full"
+          @click="$bvModal.hide('ModalNotice')"
+        >
+          닫기
+        </button>
+      </div>
+    </b-modal>
+    <!-- 모달창 끝 -->
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState, mapMutations } from 'vuex'
+
+export default {
+  layout: 'default-mo',
+
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['LOGIN']),
+    ...mapGetters(['GET_AXIOS_CALLBACK_GETTER', 'LOGIN_STUDENT']),
+  },
+  mounted() {
+    //   DATA INIT
+    console.log(this.$nuxt, this.$config)
+  },
+  methods: {
+    ...mapActions(['POST_AXIOS', 'GET_AXIOS']),
+    ...mapMutations(['LOADING_TRUE']),
+  },
+}
+</script>
+
+<style></style>
---
+++ pages-mo/test-sub-page.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="school-content">
+    <div v-if="$route.query.history" class="historyBack m-l-3 m-b-5">
+      <nuxt-link to="/">
+        <b-icon icon="arrow-left"></b-icon>
+      </nuxt-link>
+    </div>
+    <div v-if="!$route.query.history" class="historyBack m-l-3 m-b-5">
+      <b-icon icon="arrow-left" onclick="history.back()"></b-icon>
+    </div>
+    <div class="content p-l-3 p-r-3">컨텐츠</div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState, mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+
+  computed: {
+    ...mapState(['LOGIN']),
+    ...mapGetters(['GET_AXIOS_CALLBACK_GETTER', 'LOGIN_STUDENT']),
+  },
+  beforeCreate() {
+    // 인스턴스가 초기화 된 직후
+  },
+  mounted() {
+    //   DATA INIT
+    console.log(this.$nuxt, this.$config, this.rangeCalendar)
+  },
+  methods: {
+    // init
+    ...mapActions(['POST_AXIOS', 'GET_AXIOS']),
+    ...mapMutations(['LOADING_TRUE']),
+
+    // EVENT
+  },
+}
+</script>
+
+<style lang="scss"></style>
